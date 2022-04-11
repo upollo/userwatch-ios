664b8bb5f52c66a03bae5c537be03beefd7f0d00
Jan Vaughan <jvn@userwat.ch>
2022-04-11 13:13:20 +1000

    Set up UserwatchCore xcframework pod.

---
+++ UserwatchCore.xcframework/ios-arm64_x86_64-simulator/UserwatchCore.framework/Headers/UWCore.h
@@ -0,0 +1,20 @@
+#ifndef UWCore_h
+#define UWCore_h
+
+@interface UWCore : NSObject
+
+- (nullable instancetype)initWithPublicApiKey:(nonnull NSString *)key
+                             options:(nullable NSDictionary *)opts
+                               error:(NSError *_Nullable *_Nonnull)error;
+
+- (void)identifyForUid:(nullable NSString *)uid
+                 email:(nullable NSString *)email
+                 phone:(nullable NSString *)phone
+              username:(nullable NSString *)username
+                   did:(int)eventtype
+              callback:(nonnull void (^)(NSData * _Nonnull))callback
+               failure:(nonnull void (^)(NSError * _Nonnull))failure;
+
+@end
+
+#endif /* UWCore_h */
