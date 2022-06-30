510a03e8a5de58a2148cd83e403b4e41f358c5a8
Jan Vaughan <jvn@userwat.ch>
2022-06-30 11:31:17 -0700

    Version 0.1.0, with UserInfo based API and assess/track method names.

--- Userwatch/UserwatchClient.swift
+++ Userwatch/UserwatchClient.swift
@@ -4,24 +4,23 @@ import UserwatchCore
 public class UserwatchClient {
   private var core: UWCore
 
+  // Accepts the project's public api key and an optional options object.
   public init(publicApiKey: String, options: [String: String] = [:]) throws {
     self.core = try UWCore(publicApiKey: publicApiKey, options: options)
   }
 
-  public func identify(
-    uid: String?,
-    email: String?,
-    phone: String?,
-    username: String?,
+  // Inform Userwatch of an event in your client application, and get back
+  // an immediate assessment of the user.
+  //
+  // Include any UserInfo you have, or an empty UserInfo if you have none.
+  public func assess(
+    userinfo: Uwproto_UserInfo,
     did eventtype: Uwproto_EventType,
     callback: @escaping (Uwproto_AnalysisResponse) -> Void,
     failure: @escaping (Error) -> Void) {
 
-    self.core.identify(
-      forUid: uid,
-      email: email,
-      phone: phone,
-      username: username,
+    self.core.assessUser(
+      try! userinfo.serializedData(),
       did: Int32(eventtype.rawValue),
       callback: { resp in
         var ar: Uwproto_AnalysisResponse
@@ -36,25 +35,23 @@ public class UserwatchClient {
       failure: failure)
   }
 
-  public func validate(
-    uid: String?,
-    email: String?,
-    phone: String?,
-    username: String?,
+  // Inform Userwatch of an event in your client application, and get back
+  // a receipt in the form of an EventToken.
+  //
+  // Include any UserInfo you have, or an empty UserInfo if you have none.
+  public func track(
+    userinfo: Uwproto_UserInfo,
     did eventtype: Uwproto_EventType,
-    callback: @escaping (Uwproto_ValidateResponse) -> Void,
+    callback: @escaping (Uwproto_EventResponse) -> Void,
     failure: @escaping (Error) -> Void) {
 
-    self.core.validate(
-      forUid: uid,
-      email: email,
-      phone: phone,
-      username: username,
+    self.core.trackUser(
+      try! userinfo.serializedData(),
       did: Int32(eventtype.rawValue),
       callback: { resp in
-        var vr: Uwproto_ValidateResponse
+        var vr: Uwproto_EventResponse
         do {
-          vr = try Uwproto_ValidateResponse.init(serializedData: resp)
+          vr = try Uwproto_EventResponse.init(serializedData: resp)
         } catch {
           failure(error)
           return
